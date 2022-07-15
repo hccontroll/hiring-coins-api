6c2af923fc2a41b5e855a7073fdc5b0ac77f88d7
DouglasRodr <wandersondouglasr@gmail.com>
2022-07-15 19:26:01 -0300

    Primeiro Commit

---
+++ src/main/java/com/hiringcoders/HiringCoinsApiApplication.java
@@ -0,0 +1,13 @@
+package com.hiringcoders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiringCoinsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiringCoinsApiApplication.class, args);
+	}
+
+}
