2f67b7915c1cbe45a3cb6a831a49b21c29d4cfe3
DouglasRodr <wandersondouglasr@gmail.com>
2022-07-16 10:41:40 -0300

    Mensagens de Validação

--- src/main/java/com/hiringcoders/api/v1/input/TransactionInput.java
+++ src/main/java/com/hiringcoders/api/v1/input/TransactionInput.java
@@ -3,6 +3,11 @@ package com.hiringcoders.api.v1.input;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
+
 import com.hiringcoders.domain.model.TransactionType;
 
 import lombok.Getter;
@@ -11,7 +16,9 @@ import lombok.Setter;
 @Getter
 @Setter
 public class TransactionInput {
-	
+
+	@NotBlank
+	@Size(max = 40)
 	private String clientUuid;
 	
 	private String purchaseId;
@@ -20,8 +27,11 @@ public class TransactionInput {
 	
 	private OffsetDateTime purchaseDate;
 	
+	@NotNull
 	private TransactionType type;
 	
+	@NotNull
+	@Positive
 	private Long coins;
 
 }
