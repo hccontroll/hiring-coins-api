6fbbaf93e85d47dfed58d3967f9d6b8d50c4ce15
DouglasRodr <wandersondouglasr@gmail.com>
2022-07-16 10:27:09 -0300

    Ajustando Formato de Mensagem de Exceções

--- src/main/java/com/hiringcoders/domain/exception/ClientNotFoundException.java
+++ src/main/java/com/hiringcoders/domain/exception/ClientNotFoundException.java
@@ -5,7 +5,7 @@ public class ClientNotFoundException extends EntityNotFoundException {
 	private static final long serialVersionUID = 1L;
 
 	public ClientNotFoundException(String clientUuid) {
-		super(String.format("Não existe um Cliente com UUID %d", clientUuid));
+		super(String.format("Não existe um Cliente com UUID %s", clientUuid));
 	}
 
 }
