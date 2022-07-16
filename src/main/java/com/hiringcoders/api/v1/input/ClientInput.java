bcb824e0c06f864b8a62b52202f54d1f00aed820
DouglasRodr <wandersondouglasr@gmail.com>
2022-07-15 22:34:02 -0300

    Validação ClientInput

--- src/main/java/com/hiringcoders/api/v1/input/ClientInput.java
+++ src/main/java/com/hiringcoders/api/v1/input/ClientInput.java
@@ -1,5 +1,9 @@
 package com.hiringcoders.api.v1.input;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,8 +11,13 @@ import lombok.Setter;
 @Setter
 public class ClientInput {
 
+	@NotBlank
+	@Size(max = 100)
 	private String name;
 
+	@NotBlank
+	@Email
+	@Size(max = 100)
 	private String email;
 	
 }
