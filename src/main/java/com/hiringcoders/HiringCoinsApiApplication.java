4c0b9dbb47a582666f332df45a9ecba252d4b98d
DouglasRodr <wandersondouglasr@gmail.com>
2022-07-19 16:12:19 -0300

    Ajuste Erro Documentação HTTPS

--- src/main/java/com/hiringcoders/HiringCoinsApiApplication.java
+++ src/main/java/com/hiringcoders/HiringCoinsApiApplication.java
@@ -5,12 +5,16 @@ import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(servers = { @Server(url = "/", description = "Default Server URL") })
 @SpringBootApplication
 public class HiringCoinsApiApplication {
 
 	public static void main(String[] args) {
 		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
-		
+
 		SpringApplication.run(HiringCoinsApiApplication.class, args);
 	}
 
