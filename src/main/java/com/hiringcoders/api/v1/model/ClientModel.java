0fd315910e50a9ba4c182895a921aeb696d2fb59
DouglasRodr <wandersondouglasr@gmail.com>
2022-07-18 21:43:42 -0300

    Incluindo Exemplo Documentação ClientModel

--- src/main/java/com/hiringcoders/api/v1/model/ClientModel.java
+++ src/main/java/com/hiringcoders/api/v1/model/ClientModel.java
@@ -2,6 +2,7 @@ package com.hiringcoders.api.v1.model;
 
 import java.time.OffsetDateTime;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,14 +10,19 @@ import lombok.Setter;
 @Setter
 public class ClientModel {
 	
+	@Schema(example = "533988cb-8cda-49b5-8607-c53b649701e9")
 	private String uuid;
 	
+	@Schema(example = "Douglas Rodrigues")
 	private String name;
 	
+	@Schema(example = "wandersondouglasr@gmail.com")
 	private String email;
 
+	@Schema(example = "2022-07-16T13:51:05Z")
 	private OffsetDateTime lastPurchase;
 	
+	@Schema(example = "50")
 	private Long coinBalance;
 	
 }
